d6d806ed69791e88d12eb0ce1b009efef2ad275a
Jan Joska <jan@joska.net>
2020-05-28 16:25:46 +0200

    initial commit

---
+++ DemoApi/Controllers/FunctionsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class FunctionsController : ControllerBase
+    {
+        private static string ReverseString(string s)
+        {
+            var charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        [HttpGet]
+        public void Reverse([FromQuery] string input)
+        {
+            Response.WriteAsync(ReverseString(input), Encoding.UTF8);
+        }
+
+        [HttpGet]
+        public JsonResult GetVersion()
+        {
+            return new JsonResult( new {version = Environment.GetEnvironmentVariable("DEMO_API_VERSION") ?? "Unknown"});
+        }
+
+    }
+}
